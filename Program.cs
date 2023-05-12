a8350a89db5acffa41faa7d24652ad6211582914
Dylan McCarthy <dylan.v.mccarthy@gmail.com>
2023-05-12 20:33:16 +1000

    adding parameters and appsettings.json file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Net.WebSockets;
+using Microsoft.Extensions.Configuration;
 
 public class Program
 {
     public static void Main()
     {
-        Bot bot = new Bot();
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        string? endpoint = config["OpenAIEndpoint"];
+        Console.WriteLine($"Endpoint: {endpoint}");
+        string? botName = config["BotName"];
+        string? channelName = config["ChannelName"];
+        Bot bot = new Bot(endpoint, botName, channelName);
         Console.ReadLine();
     }
 }
